11bff9c61fc3b8badc3279810d45132dd404c3d2
Jaime Loeuf <jaimeloeuf@gmail.com>
2018-12-25 23:30:43 +0800

    Changes: 	Updated index.html and JS files. 	Added some basic structure to the html page and worked on the timing 	function in the JS file.

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 'use strict'; // Enforce use of strict verion of JavaScript
 
-function onClick() {
+function setTime() {
 	// Construct a duration object and convert all time to milliseconds
 	duration = {
 		's': parseInt(document.getElementById('s'), 10) * 1000, // Seconds
 		'm': parseInt(document.getElementById('m'), 10) * 1000 * 60, // Minutes
 		'h': parseInt(document.getElementById('h'), 10) * 1000 * 60 * 60, // Hours
-		'd': parseInt(document.getElementById('d'), 10) * 1000 * 60 * 60 * 24, // Days?
+		// 'd': parseInt(document.getElementById('d'), 10) * 1000 * 60 * 60 * 24, // Days?
 	}
 	let totalTime = 0;
-	duration.forEach(element => totalTime += element); // Get the total time by looping through all the keys in object
+	duration.forEach(time => totalTime += duration[time]); // Get the total time by looping through all the keys in object
 
 	setTimeout(() => {
+		// Alert the user, perhaps play a sound? To load a sound into the html page first, then play it this JS function
 		alert('Time\'s up');
-	}, time);
+	}, Math.round(totalTime)); // Time for the nearest ms
 }
 
-function resetBtn() {
+function reset() {
 	// Reset the duration inputs
 	document.getElementsByClassName('duration').map((val, index, arr) => arr[index].innerHTML = '');
 }
