903f0e62df3ddc56124ad5ad5c6b86be491108af
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-01-05 23:28:48 +0800

    Changes: 1.	Updated the css file to give the table a structure and borders
    2.	Started implementing create and delete timer functions in the js file
    	to create and delete powerzone countdown blocks inside the table.
    
    	Implemented a clear interval call to stop the timer from continuously
    	counting down and calling the timer function even after the time is 0.

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ function reset() {
 
 const setCurTime = () => (document.getElementById('cur-time').innerHTML = `The current time now is: ${Date.now()}`);
 
-async function start() {
+async function start_std_pz() {
+	new_timer()
 	await stdPZ();
 	await stdBreak();
 }
@@ -48,19 +49,29 @@ const stdBreak = () =>
 		}, 15 * 60 * 1000); // 15 min timer
 	});
 
-window.addEventListener('load', () => {
-	setCurTime(); // Set current time the moment the page loads
-	setInterval(setCurTime, 1000); // Call function to set Current Time every second
-	// countdown(Date.now() + 10000);
-	// countdown(Date.now() + 4000);
-	cool();
-});
-
 // The count down does not start immediately due to the wait. So do smth similiar to window load call.
 const countdown = (endTIme) => {
 	show_diff(endTIme - Date.now());
 	setInterval(() => show_diff(endTIme - Date.now()), 1000)
 };
+
+
+function new_timer(pz) {
+	let table = document.getElementById('PZ-table');
+	let row = table.insertRow();
+	row.insertCell().innerHTML = pz.id;
+	row.insertCell().innerHTML = pz.mins;
+	row.insertCell().innerHTML = pz.seconds;
+	row.insertCell().innerHTML = pz.state;
+}
+
+function del_timer() {
+	document.getElementById('PZ-table').deleteRow(); // Deletes last row
+	document.getElementById('PZ-table').deleteRow(0); // Deletes first row
+}
+
+
+
 
 function applyState(s, m, h) {
 	// Function for applying the time values into the elements
@@ -79,16 +90,10 @@ function set_time(type, time) {
 	}
 }
 
-function cool() {
-	var fiveMinutes = 60 * 5;
-	startTimer(fiveMinutes);
-};
-
-function startTimer(duration) {
-	let diff, minutes, seconds, start = Date.now();
+function countDown(duration) {
+	let interval, diff, minutes, seconds, start = Date.now();
 	function timer() {
-		// get the number of seconds that have elapsed since 
-		// startTimer() was called
+		// get the number of seconds that have elapsed since countDown() was called
 		diff = duration - (((Date.now() - start) / 1000) | 0);
 
 		// does the same job as parseInt truncates the float
@@ -102,18 +107,21 @@ function startTimer(duration) {
 		set_time('m', minutes);
 
 		if (diff <= 0) {
-			// add one second so that the count down starts at the full duration
-			// example 05:00 not 04:59
+			// Why is the below code there?? It does not make any sense...
+			// start = Date.now() + 1000; // add one second so that the count down starts at the full duration, e.g. 05:00 not 04:59
 
-			// Put here??
-			document.getElementById('alert').innerHTML = 'Time is up'; // Display alert
-			// Stop interval in countdown
-
-
-			start = Date.now() + 1000;
+			document.getElementById('status').innerHTML = 'Time is up'; // Display alert
+			clearInterval(interval); // Stop running the interval code by clearing it.
 		}
 	};
-	// we don't want to wait a full second before the timer starts
-	timer();
-	setInterval(timer, 1000);
+	timer(); // we don't want to wait a full second before the timer starts, thus the initial call to it.
+	interval = setInterval(timer, 1000);
 }
+
+window.addEventListener('load', () => {
+	setCurTime(); // Set current time the moment the page loads
+	setInterval(setCurTime, 1000); // Call function to set Current Time every second
+	// countdown(Date.now() + 10000);
+	// countdown(Date.now() + 4000);
+	countDown(60 * 5);
+});
