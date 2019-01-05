f81cef10c7e14adc7a079a81641cf6573d0acbe6
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-01-05 15:59:31 +0800

    Changes: 1.	Added new functions to the js file to enable display of a real time clock 	and starting of a standard powerzone the moment the window fully loads.
    2.	Edited the html page to add containers and spans areas for displaying
    	current time and more.

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 function setTime() {
 	// Construct a duration object and convert all time to milliseconds
+	// User can set time of up to seconds precision level, and up to hours
 	duration = {
 		's': parseInt(document.getElementById('s'), 10) * 1000, // Seconds
 		'm': parseInt(document.getElementById('m'), 10) * 1000 * 60, // Minutes
 		'h': parseInt(document.getElementById('h'), 10) * 1000 * 60 * 60, // Hours
-		// 'd': parseInt(document.getElementById('d'), 10) * 1000 * 60 * 60 * 24, // Days?
 	}
 	let totalTime = 0;
 	duration.forEach(time => totalTime += duration[time]); // Get the total time by looping through all the keys in object
@@ -21,3 +21,30 @@ function reset() {
 	// Reset the duration inputs
 	document.getElementsByClassName('duration').map((val, index, arr) => arr[index].innerHTML = '');
 }
+
+const setCurTime = () => (document.getElementById('cur-time').innerHTML = `The current time now is: ${Date.now()}`);
+
+function start() {
+	setCurTime(); // Set current time the moment the page loads
+	setInterval(setCurTime, 1000); // Call function to set Current Time every second
+	stdPZ().then(stdBreak);
+}
+
+const stdPZ = () =>
+	new Promise((resolve) => {
+		setTimeout(() => {
+			alert(`Time's up!`);
+			// Display time's up inside the PZ display div
+			resolve();
+		}, 45 * 60 * 1000); // 45 min timer
+	});
+
+const stdBreak = () =>
+	new Promise((resolve) => {
+		setTimeout(() => {
+			alert(`Time's up!`);
+			resolve();
+		}, 15 * 60 * 1000); // 15 min timer
+	});
+
+window.addEventListener('load', start);
